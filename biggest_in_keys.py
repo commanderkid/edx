50af8eef8094d781fa666517e03dc73b3e3be062
Alexey <nafiguspofigus@gmail.com>
2017-11-07 11:37:27 +0300

    Update biggest_in_keys.py

--- biggest_in_keys.py
+++ biggest_in_keys.py
@@ -1,6 +1,6 @@
 #biggest
 #Consider the following sequence of expressions:
-animals = {'a': ['aardvark'], 'b': ['baboon'], 'c': ['coati'], 'd': ['dog', 'donkey', 'dingo']}
+#animals = {'a': ['aardvark'], 'b': ['baboon'], 'c': ['coati'], 'd': ['dog', 'donkey', 'dingo']}
 #We want to write some simple procedures that work on dictionaries to return information.
 
 #This time, write a procedure, called biggest, which returns the key corresponding to
